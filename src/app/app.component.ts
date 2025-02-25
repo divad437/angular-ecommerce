360929499fbe90d91d6aa4b518624c3400ea1b5d
Divad <ezoodavidgael@gmail.com>
2025-02-25 06:24:22 +0100

    add routing to application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { ProductsListComponent } from "./pages/products-list/products-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, ProductsListComponent],
+  imports: [RouterOutlet, HeaderComponent],
   // 
   template: `<app-header></app-header>
-  <app-products-list></app-products-list>
   <router-outlet></router-outlet>`,
   styleUrl: './app.component.scss'
 })
