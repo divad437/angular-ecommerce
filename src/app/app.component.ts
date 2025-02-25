ff5c4571135cbc0ef45645f667ec810396c19a69
Divad <ezoodavidgael@gmail.com>
2025-02-25 04:32:16 +0100

    feat: add product list component to show all products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { ProductsListComponent } from "./pages/products-list/products-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ProductsListComponent],
   // 
-  template: '<app-header></app-header><router-outlet></router-outlet>',
+  template: `<app-header></app-header>
+  <app-products-list></app-products-list>
+  <router-outlet></router-outlet>`,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
---
+++ src/app/pages/products-list/products-list.component.ts
@@ -0,0 +1,41 @@
+import { Component, signal } from '@angular/core';
+import { Product } from '../../models/products.model';
+
+@Component({
+  selector: 'app-products-list',
+  imports: [],
+  templateUrl: './products-list.component.html',
+  styleUrl: './products-list.component.scss'
+})
+export class ProductsListComponent {
+  products = signal<Product[]>([
+    {
+      id: 1,
+      title: "Casque Bluetooth Sony WH-1000XM4",
+      image: "https://m.media-amazon.com/images/I/71o8Q5XJS5L._AC_SL1500_.jpg",
+      price: 299.99,
+      stock: 15
+    },
+    {
+      id: 2,
+      title: "Apple iPhone 15 Pro",
+      image: "https://m.media-amazon.com/images/I/71d7rfSl0wL._AC_SL1500_.jpg",
+      price: 1099.99,
+      stock: 10
+    },
+    {
+      id: 3,
+      title: "PC Portable Dell XPS 13",
+      image: "https://m.media-amazon.com/images/I/71jG+e7roXL._AC_SL1500_.jpg",
+      price: 1299.99,
+      stock: 8
+    },
+    {
+      id: 4,
+      title: "Écouteurs sans fil AirPods Pro (2e génération)",
+      image: "https://m.media-amazon.com/images/I/61f1YfTkTDL._AC_SL1500_.jpg",
+      price: 249.99,
+      stock: 20
+    }
+  ]);
+}
