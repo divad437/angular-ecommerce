89ebc46b175ac5bda4812fb60de2da6753b0cb41
Divad <ezoodavidgael@gmail.com>
2025-02-25 07:44:13 +0100

    add cart-item, order summuray and cart components to manage cart shopping

---
+++ src/app/pages/cart/cart-item/cart-item.component.ts
@@ -0,0 +1,16 @@
+import { Component, inject, input } from '@angular/core';
+import { Product } from '../../../models/products.model';
+import { ButtonComponent } from "../../../components/button/button.component";
+import { RouterLink } from '@angular/router';
+import { CartService } from '../../../services/cart.service';
+
+@Component({
+  selector: 'app-cart-item',
+  imports: [ButtonComponent],
+  templateUrl: './cart-item.component.html',
+  styleUrl: './cart-item.component.scss'
+})
+export class CartItemComponent {
+  item = input.required<Product>();
+  cartService = inject(CartService);
+}
--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -9,34 +9,12 @@ import { ProductCardComponent } from './product-card/product-card.component';
   styleUrl: './products-list.component.scss'
 })
 export class ProductsListComponent {
-  products = signal<Product[]>([
-    {
-      id: 1,
-      title: "Casque Bluetooth Sony WH-1000XM4",
-      image: "https://m.media-amazon.com/images/I/71o8Q5XJS5L._AC_SL1500_.jpg",
-      price: 299.99,
-      stock: 15
-    },
-    {
-      id: 2,
-      title: "Apple iPhone 15 Pro",
-      image: "https://m.media-amazon.com/images/I/71d7rfSl0wL._AC_SL1500_.jpg",
-      price: 1099.99,
-      stock: 10
-    },
-    {
-      id: 3,
-      title: "PC Portable Dell XPS 13",
-      image: "https://m.media-amazon.com/images/I/71jG+e7roXL._AC_SL1500_.jpg",
-      price: 1299.99,
-      stock: 8
-    },
-    {
-      id: 4,
-      title: "Écouteurs sans fil AirPods Pro (2e génération)",
-      image: "https://m.media-amazon.com/images/I/61f1YfTkTDL._AC_SL1500_.jpg",
-      price: 249.99,
-      stock: 0
-    }
-  ]);
+
+  async ngOnInit() {
+    const res = await fetch('https://fakestoreapi.com/products/category/electronics')
+    const data = await res.json()
+    this.products.set(data)
+  }
+
+  products = signal<Product[]>([]);
 }
