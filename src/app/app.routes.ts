b17f5d53d98420baedcc7ab6e6d734e0b18f19ee
Divad <ezoodavidgael@gmail.com>
2025-02-27 00:23:55 +0100

    feat: add willcard route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ProductsListComponent } from './pages/products-list/products-list.component';
 import { CartComponent } from './pages/cart/cart.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     {
@@ -11,5 +12,10 @@ export const routes: Routes = [
     {
         path: 'cart',
         component: CartComponent
+    },
+    {
+        path: '**',
+        title: "Page not found",
+        component: PageNotFoundComponent
     }
 ];
